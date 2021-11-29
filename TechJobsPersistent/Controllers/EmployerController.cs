09c50748674ad08e0ec0bc7c9c377dbd36880893
William Beisley <wbeisley@gmail.com>
2021-11-29 18:32:05 -0500

    Up to Controllers question 4

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,17 +13,29 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+           
+            return View(addEmployerViewModel);
         }
 
+        [HttpPost]
         public IActionResult ProcessAddEmployerForm()
         {
             return View();
---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+    }
+}
